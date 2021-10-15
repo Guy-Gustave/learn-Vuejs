f2b5238166d8335426095403506ecf87a50f290a
Guy <guymoustapha@gmail.com>
2021-10-15 09:29:46 +0200

    display currencies

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <currencies/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+import Currencies from '../components/Currencies.vue';
+export default {
+  name: "About",
+  components: {
+    Currencies
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/Currencies.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    // Currencies,
   },
 };
 </script>
